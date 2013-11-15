f16c2b194974d350e524bead471709848e0745c4
bluedragonz <brian@seoette.com>
2013-11-15 05:34:35 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var app = express.createServer();
+var socket = require('socket.io');
+app.configure(function(){
+  app.use(express.static(__dirname + '/'));
+});
+
+var port = 3333;
+
+app.use(express.cookieParser());
+app.use(express.session({secret: 'secret', key: 'express.sid'}));
+
+app.configure('development', function() {
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function() {
+  app.use(express.errorHandler());
+});
+
+app.get('/', function(req, res){
+  res.render('index', {
+    title: 'title'
+  });
+});
+
+var server = app.listen(port);
+var io = socket.listen(server);
+
+var active_connections = 0;
+io.sockets.on('connection', function (socket) {
+
+  active_connections++
+
+  io.sockets.emit('user:connect', active_connections);
+
+  socket.on('disconnect', function () {
+    active_connections--
+    io.sockets.emit('user:disconnect', active_connections);
+  });
+
+});

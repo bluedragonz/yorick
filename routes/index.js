84de6b82b52d5059999f32c82531ab841a2026db
bluedragonz <brian@seoette.com>
2013-11-16 03:38:10 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ var module_race = require("../lib/modules/yorick");
 
 exports.configure = function (app){
         app.get('/', function(req, res){
-                res.render ('index.php', {title: 'Yorick'});
+                res.render ('index.html', {title: 'Yorick'});
         });
 }

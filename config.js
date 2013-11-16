70b67c5d3d6b2549485d8ecd104a3512d6df047e
bluedragonz <brian@seoette.com>
2013-11-15 21:36:35 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+exports.values={
+        version: '0.0.1',
+        server : {
+                production : {
+                        real_time_server : {port: 3333, host: '127.0.0.1'}
+                }
+        }
+        ,
+        game : {
+                duration: 30
+        }
+}
